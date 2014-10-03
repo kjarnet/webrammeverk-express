63373f3d759e585fb5cdd8012a5e87789a6b0413
Oystein <gelefisk@gmail.com>
2014-10-04 00:01:49 +0200

    Added tests for routes with supertest.

--- test/routes.js
+++ test/routes.js
@@ -1,10 +1,33 @@
 var expect = require("chai").expect;
+var request = require("supertest");
+var app = require("../todoapp.js");
+
 describe('Array', function(){
   describe('#indexOf()', function(){
     it('should return -1 when the value is not present', function(){
       expect([1,2,3].indexOf(5)).to.equal(-1);
       expect([1,2,3].indexOf(0)).to.equal(-1);
-    })
-  })
-})
+    });
+  });
+});
+
+describe('GET /', function(){
+    it('respond with html', function(done){
+        request(app)
+        .get('/')
+        .set('Accept', 'text/html')
+        .expect('Content-Type', /html/)
+        .expect(200, done);
+    });
+});
+
+describe('GET /users', function(){
+    it('respond with json', function(done){
+        request(app)
+        .get('/user')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200, done);
+    });
+});
 
