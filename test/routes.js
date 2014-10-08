82eef414cadfa0f48a0ead65d34cbc9e9ef5fda8
Oystein <gelefisk@gmail.com>
2014-10-08 11:02:59 +0200

    Added jshint-settings.

--- test/routes.js
+++ test/routes.js
@@ -1,33 +1,50 @@
+/*globals describe:false, it:false */
 var expect = require("chai").expect;
 var request = require("supertest");
 var app = require("../todoapp.js");
 
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      expect([1,2,3].indexOf(5)).to.equal(-1);
-      expect([1,2,3].indexOf(0)).to.equal(-1);
-    });
-  });
-});
+describe('Routes', function () {
 
-describe('GET /', function(){
-    it('respond with html', function(done){
-        request(app)
-        .get('/')
-        .set('Accept', 'text/html')
-        .expect('Content-Type', /html/)
-        .expect(200, done);
-    });
-});
+    'use strict';
 
-describe('GET /todos', function(){
-    it('respond with json', function(done){
-        request(app)
-        .get('/todos')
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(200, done);
+    describe('Array', function () {
+        describe('#indexOf()', function () {
+            it('should return -1 when the value is not present', function () {
+                expect([1, 2, 3].indexOf(5)).to.equal(-1);
+                expect([1, 2, 3].indexOf(0)).to.equal(-1);
+            });
+        });
     });
+
+    describe('GET /', function () {
+        it('respond with html', function (done) {
+            request(app)
+                .get('/')
+                .set('Accept', 'text/html')
+                .expect('Content-Type', /html/)
+                .expect(200, done);
+        });
+    });
+
+    describe('GET /todos', function () {
+        it('respond with json', function (done) {
+            request(app)
+                .get('/todos')
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/)
+                .expect(200, done);
+        });
+    });
+
+    describe('PUT /todos/:id', function () {
+        it('respond with json', function (done) {
+            request(app)
+                .put('/todos')
+                .set('Accept', 'application/json')
+                .expect('Content-Type', /json/)
+                .expect(200, done);
+        });
+    });
+
 });
 
